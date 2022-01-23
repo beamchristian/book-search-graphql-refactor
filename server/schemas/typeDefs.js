6d825c790b044086aed25a288f1d1e7132f534b0
Christian Beam <beamchristian@yahoo.com>
2022-01-23 16:01:19 -0500

    books successfully saving

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -42,7 +42,7 @@ const typeDefs = gql`
   type Mutation {
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
-    saveBook(input: BookInput): User
+    saveBook(book: BookInput): User
     removeBook(bookId: String!): User
   }
 `;
